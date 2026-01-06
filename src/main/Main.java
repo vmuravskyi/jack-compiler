3d01f72983dc3472fda63572c2338d6285886dba
Volodymyr <volodymyr@Volodymyrs-MacBook-Air.local>
2026-01-06 18:19:12 +0100

    project 10 - Jack Analyzer

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,19 @@
 package main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Example input:
+        // args[0] = "projects/10/Square"
+        // args[0] = "projects/10/Square/Main.jack"
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if (args.length != 1) {
+            System.err.println("Usage: java Main <source>");
+            System.err.println("  <source> = .jack file OR directory containing .jack files");
+            System.exit(1);
         }
-    }
 
+        // Delegate everything to JackAnalyzer
+        JackAnalyzer.main(args);
+    }
 }
